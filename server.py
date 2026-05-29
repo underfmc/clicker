394e29fea3e11bc629f0dd65ba30ab6aac425f83
Сергей Яковлев <lol0707h@gmail.com>
2026-05-29 09:55:12 +0500

    v1.0

---
+++ server.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+scores = {"team1": 0, "team2": 0}
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        await websocket.send_json(scores)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: dict):
+        for connection in self.active_connections:
+            try:
+                await connection.send_json(message)
+            except:
+                pass
+
+manager = ConnectionManager()
+
+@app.get("/")
+async def get_index(request: Request):
+    return templates.TemplateResponse(request, "index.html")
+
+@app.get("/results")
+async def get_results(request: Request):
+    return templates.TemplateResponse(request, "results.html")
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if data in scores:
+                scores[data] += 1
+                await manager.broadcast(scores)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
